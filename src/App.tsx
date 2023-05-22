84be31d5f724b864775736b8817070521c2762ee
ankitSuryawanshi <suryawanshiankit97@gmail.com>
2023-05-22 12:25:25 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      App component is here...
+    </div>
+  );
+}
+
+export default App;
