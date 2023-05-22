5f5c7b830d44ef083faf3c1bb2abcd5325396065
ankitSuryawanshi <suryawanshiankit97@gmail.com>
2023-05-22 15:21:24 +0530

    created heading and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="text-center	">
-      App component is here...
+      <Header />
+      <div
+        className="bg-neutral-50 px-6 py-20 text-center text-neutral-800 dark:bg-neutral-700 dark:text-neutral-200"
+      >
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2">
+            Show All Appointments
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-2">
+            Create Appointments
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav
+        class="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+        data-te-navbar-ref>
+        <div class="flex w-full flex-wrap items-center justify-between px-3">
+          <div
+            class="!visible hidden grow basis-[100%] items-center lg:!flex lg:basis-auto"
+          >
+            Select your appointment time.
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
